caa57348fb21724ce8961a68e7270c0d288bfb65
kirill <kirill@gmail.com>
2025-11-30 10:44:11 +0300

    Добавьте файлы проекта.

---
+++ Vionis/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Vionis
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Vionis/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "Vionis")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "Vionis.Pages")]
